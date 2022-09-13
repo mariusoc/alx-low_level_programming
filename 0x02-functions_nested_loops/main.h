dfd3f35e784579b6cc7cd349ca581c56a0bb81ae
mariusoc <nonsookeke@gmail.com>
2022-09-13 16:02:30 -0700

    new loop

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,35 @@
-#include "main.h"
-/** * main - prints the word _putchar * * Return: 0 on success execution. */
-int main(void){	char s[] = "_putchar";	int i = 0;
-	while (s[i] != '\0')	{		_putchar(s[i]);		i++;	}	_putchar(10);
-	return (0);}
+#ifndef MAIN_H_INCLUDED
+
+#define MAIN_H_INCLUDED
+
+
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+
+
+#endif
